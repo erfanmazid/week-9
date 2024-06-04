45d7f189d295144842fb94dbdcd41516ca4dd43c
erfan mazidabadi <erfanmazidam@gmail.com>
2024-06-04 10:00:20 +0330

    get country flag

--- question1/script.js
+++ question1/script.js
@@ -2,3 +2,21 @@ const container = document.querySelector(".container");
 
 const input = document.getElementById("input");
 const btn = document.getElementById("btn");
+
+function fetchCountry(countryName) {
+  fetch(`https://restcountries.com/v3.1/name/${countryName}?fullText=true`)
+    .then((response) => response.json() || console.log(response))
+    .then((datas) => renderCountry(datas));
+}
+
+function renderCountry(country) {
+  // console.log("clicked");
+  // console.log(country);
+
+  const flag = country[0].flags.png;
+}
+
+btn.addEventListener("click", () => {
+  fetchCountry(input.value);
+});
+fetchCountry();
