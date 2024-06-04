9c4a59cbde057e863a11d5f7b4ae83774637a12b
erfan mazidabadi <erfanmazidam@gmail.com>
2024-06-04 15:53:08 +0330

    add new user and delete

--- question2/script.js
+++ question2/script.js
@@ -1,19 +1,26 @@
 const baseUrl = "http://localhost:3000";
 
-const name = document.getElementById("name");
-const email = document.getElementById("email");
+const userName = document.getElementById("name");
+const userEmail = document.getElementById("email");
 const submit = document.getElementById("submit");
 
 const tbody = document.querySelector("tbody");
+
+let userNum = 0;
+
+fetch(`${baseUrl}/users`)
+  .then((res) => res.json())
+  .then((data) => (userNum = data.length));
 
 function fetchUser() {
   fetch(`${baseUrl}/users`)
     .then((res) => res.json())
     .then((data) => renderUser(data));
-  console.log("mmd");
 }
 
 function renderUser(users) {
+  tbody.innerHTML = "";
+
   users.forEach((user) => {
     const bodyTr = document.createElement("tr");
 
@@ -21,7 +28,7 @@ function renderUser(users) {
     idTd.textContent = user.id;
 
     const nameTd = document.createElement("td");
-    nameTd.textContent = user.first_name + " " + user.last_name;
+    nameTd.textContent = user.first_name;
 
     const emailTd = document.createElement("td");
     emailTd.textContent = user.email;
@@ -29,6 +36,7 @@ function renderUser(users) {
     const btnTd = document.createElement("td");
     const btn = document.createElement("button");
     btn.textContent = "delete";
+    btn.setAttribute("onclick", `deleteUser(${user.id})`);
     btnTd.appendChild(btn);
 
     bodyTr.appendChild(idTd);
@@ -39,4 +47,54 @@ function renderUser(users) {
     tbody.appendChild(bodyTr);
   });
 }
+
+function addUser() {
+  const name = userName.value;
+  const email = userEmail.value;
+
+  if (name && email) {
+    const newUser = {
+      first_name: name,
+      email: email,
+      id: `${userNum + 1}`,
+    };
+
+    submit.textContent = "Adding User ....";
+    submit.disabled = true;
+
+    fetch(`${baseUrl}/users`, {
+      method: "POST",
+      body: JSON.stringify(newUser),
+      headers: {
+        "Content-type": "application/json",
+      },
+    });
+
+    cleanForm();
+
+    submit.textContent = "Add User";
+    submit.disabled = false;
+
+    fetchUser();
+  }
+}
+
+function cleanForm() {
+  name.value = "";
+  email.value = "";
+}
+
+function deleteUser(id) {
+  fetch(`${baseUrl}/users/${id}`, {
+    method: "DELETE",
+  });
+
+  fetchUser();
+}
+
+submit.addEventListener("click", (e) => {
+  e.preventDefault();
+  addUser();
+});
+
 fetchUser();
